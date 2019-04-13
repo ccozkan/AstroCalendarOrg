93378baa21874d9e3cdbab17670339f90ed8a45e
Çağrı Özkan <cc.ozkan@gmail.com>
2019-04-13 15:50:31 +0300

    inicom

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # AstroCalendarOrg
-converts astronomic events calendar from seasky.org to rg mode file and possibly to icalendar ics file
+converts astronomic events calendar from seasky.org to org mode file and possibly to icalendar (ics) file
+
+
+This project is inspired from kroeliebuschie/AstroCalendar. There was some bug when I was trying to convert ics to org file. I couldn't fix it, so I wrote this one. 
+## usage
+
+Make sure you have python3 and requests, bs4, datetime, pytz packages are installed.
+Alse modify the timezone in line 9, if you want it to be accurate.
+
+ 
+Following command creates the org file
+
+```bash
+python3 astro.py
+```
+
+## ics file
+
+Emacs has an icalendar file export.
+
+## future work
+
+i may implement multiple year and multiple event day support, i may not.
+
+## be aware
+
+the content on seaksy.org is copyrighted.
+
+>Information contained within this site may be used for any personal, educational, and most non-commercial purposes as long as Sea and Sky is credited as the source.
---
+++ astro.py
@@ -0,0 +1,111 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+import pytz 
+
+r = requests.get('http://www.seasky.org/astronomy/astronomy-calendar-2019.html')
+soup = BeautifulSoup(r.text,'html.parser')
+
+tz = pytz.timezone("Europe/Istanbul")
+
+
+event_date = soup.findAll('span',{'class':'date-text'})
+event_title = soup.findAll('span',{'class':'title-text'})
+event_title = soup.findAll('p')
+
+events = event_title[9:-12]
+
+def month2num(month):
+    if month == 'January':
+        return 1
+    elif month == 'February':
+        return 2
+    elif month == 'March':
+        return 3
+    elif month == 'April':
+        return 4
+    elif month == 'May':
+        return 5
+    elif month == 'June':
+        return 6
+    elif month == 'July':
+        return 7
+    elif month == 'August':
+        return 8
+    elif month == 'September':
+        return 9
+    elif month == 'October':
+        return 10
+    elif month == 'November':
+        return 11
+    elif month == 'December':
+        return 12
+dicto = {}
+dicto['2019'] = {}
+
+for event in range(0,len(events)):
+    #print(event)
+    dt = events[event].text.split('- ',1)[0]
+    kalan = events[event].text.split('- ',1)[1]
+    t = kalan.split('. ',1)[0]
+    d = kalan.split('. ',1)[1]
+
+    m = month2num(dt.split(' ',1)[0])
+
+    day = dt.split(' ',1)[1]
+    days = []
+
+    if ', ' or ' & ' in day:
+        day.replace('&',',')
+        
+        day = day.split(', ',1)[0]
+        day = day.split(' & ',1)[0]
+#    if ',' in day:
+#        day = day.split(', ',1)[0]
+#        for i in range(0,day.count(', ')):
+#            days.append(day.split(', ',day.count(', '))[i])
+#    else:
+#        days.append(day)
+        #print(day)
+
+
+
+    with open("x_astrocal19.org", "a") as myfile:
+        myfile.write('* ' + t +'\n')
+
+
+    if ' UTC' in d:
+        tutc = d.split(' UTC')[0][-5:]
+        #print(tutc)
+
+        dobj = tz.localize(datetime(2019, m, int(day), int(tutc[:2]), int(tutc[-2:])), is_dst=None)
+
+        with open("x_astrocal19.org", "a") as myfile:
+            myfile.write('   <' + dobj.strftime('%Y-%m-%d %a %H:%M') + '>\n')
+#    if ',' in day:
+#        day = day.split(', ',1)[0]
+#        for i in range(0,day.count(', ')):
+#            days.append(day.split(', ',day.count(', '))[i])
+#    else:
+#        days.append(day)
+        #print(day)
+
+    else:
+        #print(day)
+        dobj = datetime(2019,m,int(day))
+
+        with open("x_astrocal19.org", "a") as myfile:
+            myfile.write('   <' + dobj.strftime('%Y-%m-%d %a') + '>\n')
+       
+
+
+    
+    with open("x_astrocal19.org", "a") as myfile:
+        myfile.write(dt + '\n' + d + '\n')
+
+        
+    with open("x_astrocal19.org", "a") as myfile:
+        myfile.write('(copyright: seasky.org)'+ '\n')
+
+
+
